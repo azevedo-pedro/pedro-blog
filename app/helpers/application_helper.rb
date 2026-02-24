e99c31c694f0b3faac00544d6c2713b7abe4c730
Pedro Azevedo <azevedodev@outlook.com>
2026-02-24 01:42:43 -0300

    Add Portuguese date formatting helper and update post date displays

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  MONTHS_PT = %w[
+    janeiro fevereiro março abril maio junho
+    julho agosto setembro outubro novembro dezembro
+  ].freeze
+
+  def format_date_pt(date)
+    "#{date.day} de #{MONTHS_PT[date.month - 1]} de #{date.year}"
+  end
 end
